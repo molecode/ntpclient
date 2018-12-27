7420e75a53affadafdf69311327c1e9be038ef15
Timo <info@timoschaepe.de>
2018-12-28 00:02:33 +0100

    use struct for unpacking the response

--- python/ntpclient.py
+++ python/ntpclient.py
@@ -1,11 +1,19 @@
+import struct
 import socket
+from datetime import datetime
 
 ntp_server = 'pool.ntp.org'
 ntp_port = 123
+
+# unix epoch starts at 1970-01-01, ntp epoch at 1900-01-01, so we need the difference
+ntp_epoch_offset = 2208988800
 message = b'\x1b' + 47 * b'\00'
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto(message, (ntp_server, ntp_port))
 data = sock.recv(1024)
 
-print(data)
+sec = struct.unpack('>12I', data)[10]
+current_time = sec - ntp_epoch_offset
+
+print(datetime.utcfromtimestamp(current_time))
